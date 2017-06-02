3e4b1927919bf7bea93457e5708b46b91a11ad8b
Advaith Jagathesan <advaithj@zoobuh.com>
2017-06-02 15:38:01 -0700

    Re-upload

---
+++ bin/Release/AppX/js/Extensions.js
@@ -0,0 +1,48 @@
+﻿//this file gets generated at compile time and are referenced by the runtime
+
+CCompiledExtensions = {};
+
+CCompiledExtensions.createList = function () {
+    //must be called via blah.call(this);
+
+    var extMaxHandle = 6;
+    if (extMaxHandle) {
+        this.extensions = new Array(extMaxHandle);
+        this.numOfConditions = new Array(extMaxHandle);
+        var n;
+        for (n = 0; n < extMaxHandle; n++) {
+            this.extensions[n] = null;
+            this.numOfConditions[n] = 0;
+        }
+
+        var e;
+
+        // START_ADDEXT
+            e = new CExtLoad();
+            e.handle = 0;
+            this.addExt(e);
+            e = new CExtLoad();
+            e.handle = 3;
+            this.addExt(e);
+            e = new CExtLoad();
+            e.handle = 5;
+            this.addExt(e);
+        // INCLUDE_ADDEXT
+    }
+};
+
+CCompiledExtensions.loadRunObject = function () {
+    //must be called via blah.call(this);
+    switch (this.handle) {
+        // START_NEWEXT
+        case 0:
+            return new CRunkchisc();
+        case 3:
+            return new CRunHTML5();
+        case 5:
+            return new CRunMobileFont();
+        // INCLUDE_NEWEXT
+    }
+
+    return null;
+};
---
+++ bin/Release/AppX/js/Movements.js
@@ -0,0 +1,13 @@
+﻿//this file gets generated at compile time and are referenced by the runtime
+
+CCompiledMovements = {};
+
+CCompiledMovements.loadMvtExtension = function (extName) {
+    //must be called via blah.call(this);
+
+    var object = null;
+
+    
+
+    return object;
+};
